6ea58e00ea26e2e6365a495b380d2c7b3592bc54
dylan-turner25 <dylan.turner25@uga.edu>
2021-09-15 08:48:24 -0400

    defining the valid_parameters() function.

---
+++ R/valid_parameters.R
@@ -0,0 +1,18 @@
+
+
+#' Get all valid API parameters for a given FEMA data set
+#'
+#' @param data_set A character string indicating the data set to return valid parameters for
+#'
+#' @return Returns a vector of parameter names that can be used to filter an API call for a given open FEMA dataset
+#' @export
+#'
+#' @examples
+#' valid_parameters("fimaNfipPolicies")
+
+valid_parameters <- function(data_set = NULL){
+  data_fields <- fema_data_fields(data_set) 
+  data_fields <- data_fields[which(data_fields$`Is Searchable` == "yes"),] # remove rows for parameters that aren't searchable
+  params <- data_fields$Name
+  return(params)
+}

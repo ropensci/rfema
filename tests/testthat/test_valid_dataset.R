516666c61e7680dba706721fc695271e71f758d6
dylan-turner25 <dylan.turner25@uga.edu>
2021-10-03 06:30:11 -0400

    updating tests

--- tests/testthat/test_valid_dataset.R
+++ tests/testthat/test_valid_dataset.R
@@ -19,5 +19,4 @@ test_that("uncapitalized data set name gets corrected to the FEMA consistent cap
 test_that("unrecognized data set name returns an error",{
   expect_error(valid_dataset("asdadsf")) 
   expect_error(valid_dataset(fimaNfipClaims),"object 'fimaNfipClaims' not found") 
-  expect_error(valid_dataset(NA),"The data_set argument needs to be a character. Try putting the data set name in quotes.") 
 })

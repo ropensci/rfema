c8b0e1acbf1e459c57946e17fe1f3df2b2375438
dylan-turner25 <dylan.turner25@uga.edu>
2021-10-07 15:24:33 -0400

    making sure parameter_values() doesn't return factors.

--- R/parameter_values.R
+++ R/parameter_values.R
@@ -11,5 +11,9 @@
 parameter_values <- function(data_set = NULL, data_field = NULL) {
   data <- openFema(data_set = data_set, top_n = 999, select = data_field, ask_before_call = F)
   values <- unique(data[, data_field])
+  
+  # convert everything to a character to avoid returning factors
+  values <- as.character(values)
+  
   return(values)
 }

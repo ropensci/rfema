7e832a144db05c011d564934c2c233cedc07e0d4
dylan-turner25 <dylan.turner25@uga.edu>
2021-10-11 05:56:51 -0400

    adding more tests

--- tests/testthat/test_bulk_dl.R
+++ tests/testthat/test_bulk_dl.R
@@ -8,4 +8,38 @@ test_that("downloaded file is in the specified location", {
 })
 
 # remove file so directory doesn't get cluttered
+file.remove("test_dl.csv")
+
+
+
+
+
+# bulk download one of the smaller data sets without specifying download details
+bulk_dl("femaregions", output_dir = getwd(), file_name = "test_dl.csv")
+
+ # check to see if file is in the folder
+ test_that("downloaded file is in the cd if no location is specified", {
+   expect_equal("test_dl.csv" %in% list.files(getwd()),T)
+ })
+ 
  file.remove("test_dl.csv")
+ 
+ 
+ 
+ 
+ # download without specifying a file name
+ ds <- "femaregions"
+ bulk_dl(ds)
+ 
+ # get name of csv files/rds files in wd
+ csv_file <- list.files(getwd(), pattern = ".csv")
+
+ # check to see if file is in the folder
+ test_that("bulk_dl works without specifying file path or file name", {
+   expect_equal( grepl(paste0(valid_dataset(ds), "_", Sys.Date()), csv_file), T)
+ })
+ 
+ file.remove(csv_file)
+ 
+
+ 
--- tests/testthat/test_openFema.R
+++ tests/testthat/test_openFema.R
@@ -16,3 +16,38 @@ for (data_set in data_sets$name[c(1, 4, 6, 12, 16)]) {
     expect_error(openFema(data_set, top_n = 0), "Setting top_n = 0 wont return any records. Set top_n to a value greater than 0")
   })
 }
+
+
+
+# test save to file functionality for csv files
+
+# download file 
+ds <- "femaregions" # using femaregions because it is a small file
+openFema(data_set = ds, file_type = "csv")
+openFema(data_set = ds, file_type = "rds")
+
+# get name of csv files/rds files in wd
+csv_file <- list.files(getwd(), pattern = ".csv")
+rds_file <- list.files(getwd(), pattern = ".rds")
+
+
+# check to see if file is in the folder
+test_that("downloaded file is in the specified location with correct name", {
+  expect_equal( grepl(paste0(ds, "_", Sys.Date()), csv_file), T)
+  expect_equal( grepl(paste0(ds, "_", Sys.Date()), rds_file), T)
+})
+
+# remove file so directory doesn't get cluttered
+file.remove(csv_file)
+file.remove(rds_file)
+
+
+
+
+
+
+
+
+
+
+

be5668ef1bae7f4acf23c356aa442f4008b06f55
dylan-turner25 <dylan.turner25@uga.edu>
2021-10-29 08:11:37 -0400

    moving more tests over to vcr/webmockr framework.

--- tests/testthat/test_fema_api_endpoints.R
+++ tests/testthat/test_fema_api_endpoints.R
@@ -1,15 +1,15 @@
-# get all data sets from the API so that all the data sets can be tested
-# automatically with a loop
-data_sets <- fema_data_sets()
-
-# loop over all data set names and perform the same tests
-for (data_set in data_sets$name) {
   test_that("returned object is a character", {
-    expect_equal(is.character(fema_api_endpoints(data_set)), T)
+    vcr::use_cassette("api_endpoint", {
+      returned_obj <- fema_api_endpoints("fimanfipclaims")
+    })
+    expect_equal(is.character(returned_obj), T)
   })
 
 
   test_that("first part of returned character matches the fema API url", {
-    expect_equal(substr(fema_api_endpoints(data_set), 1, 24), "https://www.fema.gov/api")
+    vcr::use_cassette("api_endpoint", {
+      returned_obj <- fema_api_endpoints("fimanfipclaims")
+    })
+    expect_equal(substr(returned_obj, 1, 24), "https://www.fema.gov/api")
   })
-}
+
--- tests/testthat/test_parameter_values.R
+++ tests/testthat/test_parameter_values.R
@@ -1,18 +1,15 @@
 
 
 test_that("Error is returned when data field is not in the data set", {
+  vcr::use_cassette("non_existing_dataset", {
   expect_error(parameter_values(data_set = "fimanfipclaims", data_field = "asdfasdf"))
+  })
 })
 
-
-# get all data sets to test multiple automatically through a loop
-data_sets <- fema_data_sets()
-
-# loop over some data sets (this test takes a while so limiting a rew random data sets)
-for (data_set in data_sets$name[c(1, 3, 4, 7, 19)]) {
-  print(data_set)
-  test_that(paste0("check returned object is a character vector of dimensions 1 x N for data set:", data_set), {
-      returned_obj <- parameter_values(data_set, valid_parameters(data_set)[1])
-    expect_equal(is.vector(returned_obj), TRUE)
+test_that(paste0("returned object is a character vector of dimensions 1 x N"), {
+  vcr::use_cassette("parameter_value", {
+    returned_obj <- parameter_values("fimanfipclaims", "floodZone")
   })
-}
+  expect_equal(is.vector(returned_obj), TRUE)
+})
+
